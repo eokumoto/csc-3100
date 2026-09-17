2c110c7dad61bcb79f28f6203cc657fdcaf8b44a
eokumoto <eokumoto28@gmail.com>
2026-09-16 19:15:38 -0700

    IE3 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,7 +7,11 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function removeOneCharacter(index) {
@@ -28,6 +32,18 @@ function MyApp() {
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
     return promise;
   }
 
